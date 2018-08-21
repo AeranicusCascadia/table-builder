922b3513a99c496c39a6a06d66e01f5fdc74fb68
AeranicusCascadia <35699755+AeranicusCascadia@users.noreply.github.com>
2018-08-21 11:30:18 -0700

    clean up and add extensive comments main.js

--- main.js
+++ main.js
@@ -1,19 +1,38 @@
 // Main javascript
 
-
+// Disable event: focus on press 'Enter'
 function stopRKey(evt) { 
   var evt = (evt) ? evt : ((event) ? event : null); 
   var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null); 
   if ((evt.keyCode == 13) && (node.type=="text"))  {return false;} 
 } 
-
 document.onkeypress = stopRKey
 
+// HIDE FUNCTIONS
 
-function hideButton() {
-	document.getElementById("tableGenerator").hidden = true;
+// Hide tableGenerator button 
+function hideTableGeneratorButton() {
+	document.getElementById("tableGeneratorButton").hidden = true;
 }
 
+// Hides input forms and button
+function hideInputsDiv() {
+	document.getElementById("inputs").hidden = true;
+}
+
+// SHOW FUNCTIONS
+
+// show table generator button
+function showtableGeneratorButton() {
+document.getElementById("tableGeneratorButton").hidden = false;
+}
+
+// Wrapper function to call onload functions
+function pageLoadFunctions() {
+	hideTableGeneratorButton();
+}
+
+// create object: myTable
 var myTable = {
 	title: 'untitled',
 	subtitle: 'none',
@@ -21,21 +40,14 @@ var myTable = {
 	cols: 0
 }
 
-function setRowsCols() {
-	var num_rows = document.getElementById("choose_rows").value;
-	var num_cols = document.getElementById("choose_cols").value;
-
-	myTable.rows = parseInt(num_rows)+1; 
-	myTable.cols = num_cols;
-}
-
+// Checks contents of fields: choose_rows, choose_cols for valid number.
 
 function validateRowsCols() {
 	val_rows = document.getElementById("choose_rows").value;
 	val_cols = document.getElementById("choose_cols").value;
 	
-	if (isNaN(parseInt(val_rows)) || isNaN(parseInt(val_cols))) {
-		window.alert("Please enter only whole numbers for rows and columns.");
+	if (isNaN(parseInt(val_rows)) || isNaN(parseInt(val_cols))) { // If check fails, reloads page
+		window.alert("Please enter only numbers for rows and columns.");
 		location.reload();
 	} else {
 		// window.alert("All numbers.");
@@ -43,46 +55,52 @@ function validateRowsCols() {
 	}
 }
 
+// Takes form input to declare initial number of rows and columns
+function setRowsCols() {
+	var num_rows = document.getElementById("choose_rows").value;
+	var num_cols = document.getElementById("choose_cols").value;
+
+	// Modifies myTable properties based on validated form input data
+	myTable.rows = parseInt(num_rows)+1; // extra row created for column headers
+	myTable.cols = parseInt(num_cols);
+}
 
 function buildInitialEmptyTable() {
-	// target div
+	
+	// create local var 'table' and assign div element 'my_table' to it
 	var table = document.getElementById("my_table");
 	
-	for (i = 0; i < myTable.rows; i++) { // changed i from 0
-	var row,
-		cell;
-		if (i==0){
-			console.log(i);
-			row = table.insertRow(i);
-			row.classList.add("first");
-			console.log(row);
-			for (j = 0; j < myTable.cols; j++) {
-				cell = row.insertCell(j);
-				cell.innerHTML = '<textarea></textarea>';
-				
-				// working code:
-				// cell.innerHTML = '<form><input type="text"></form>'; 
-			}
-		}else{
-			row = table.insertRow(i);
+	for (i = 0; i < myTable.rows; i++) { // Outer loop: iterate based on 'row's property of 'myTable' object
+	
+	var row; // declare local variables 'row' and 'cell'
+	var cell;
 		
-			for (j = 0; j < myTable.cols; j++) {
-				cell = row.insertCell(j);
-				cell.innerHTML = '<textarea></textarea>';
-				
-				// working code:
-				// cell.innerHTML = '<form><input type="text"></form>'; 
+		// Outer loop iterates rows
+		if (i==0){ // If this is the first iteration of outer loop (first row, column header):
+			
+			row = table.insertRow(i); // row variable holds result of calling insertRow method on table
+			row.classList.add("first"); // Assign a new CSS class to row variable.
+			
+			// Inner loop iterates colums (cells per row)
+			for (j = 0; j < myTable.cols; j++) { // Iterate loop of how many cells to insert in first row based on myTable.cols
+				cell = row.insertCell(j); // call insert cell method on row variable at 'j' position of row.
+				cell.innerHTML = '<textarea></textarea>'; // pass hmtl contents to cell
 			}
-		
+		}else{ // if it's not the first iteration of outer loop (first row):
+			// row variable holds result of calling insertRow method on table at outer loop index, same as above.
+			row = table.insertRow(i); 
+			
+			// Inner loop iterates colums (cells per row)
+			for (j = 0; j < myTable.cols; j++) { // Iterate based on myTable.cols
+				cell = row.insertCell(j); // Insert cell into row at 'j' position 
+				cell.innerHTML = '<textarea></textarea>'; // // pass hmtl contents to cell
+			}
 		}
 	}
 		
-	// Hides input forms and button
-	document.getElementById("inputs").hidden = true;
-	// Shows generate graphic button
-	document.getElementById("tableGenerator").hidden = false;
+	hideInputsDiv() // hide 'inputs' div
+	showtableGeneratorButton() // show table generator button
 }
-
 
 function addRowsCols() {
 	validateRowsCols();
@@ -94,23 +112,3 @@ function generatePNG() {
 	takeScreenShot();
 	hideButton();
 }
-
-/*
-// Example funtion for adding rows and cells
-function addRow() {
-	// Find a <table> element with id
-	var table = document.getElementById("blank_table");
-
-	// Create an empty <tr> element and add it to the 1st position of the table:
-	var row = table.insertRow(0);
-
-	// Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
-	var cell1 = row.insertCell(0);
-	var cell2 = row.insertCell(1);
-
-	// Add some text to the new cells:
-	cell1.innerHTML = "NEW CELL1";
-	cell2.innerHTML = "NEW CELL2";
-}
-*/
-	
